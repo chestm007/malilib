e04b0dca317a5c61cb7f0349c78f359e7fcb3d0b
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2023-12-25 20:45:14 +0200

    Switch to a client init entrypoint

--- src/main/java/malilib/MaLiLib.java
+++ src/main/java/malilib/MaLiLib.java
@@ -1,15 +1,15 @@
 package malilib;
 
-import net.ornithemc.osl.entrypoints.api.ModInitializer;
+import net.ornithemc.osl.entrypoints.api.client.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class MaLiLib implements ModInitializer
+public class MaLiLib implements ClientModInitializer
 {
     public static final Logger LOGGER = LogManager.getLogger(MaLiLibReference.MOD_ID);
 
     @Override
-    public void init()
+    public void initClient()
     {
     }
 
